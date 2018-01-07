812c0da66c9fc0d25d803d49b02e2613c97d3f4d
bahrus <anderson.bruce.b@gmail.com>
2018-01-07 13:26:23 -0500

    xtal-pattern

---
+++ build/ES6/xtal-pattern.js
@@ -0,0 +1,19 @@
+(function(){class a extends HTMLElement{connectedCallback(){this._href=this.getAttribute('href');const a=this._href.split('/');this._fileName=a[a.length-1].replace('.html',''),this.loadURL()}loadURL(){fetch(this._href).then((a)=>{const b=document.createElement('script'),c=this._fileName.replace('-','_');b.innerText=`
+                    (function () {
+                        class ${c} extends Polymer.Element{
+                            static get is(){return '${this._fileName}'}
+                            static get properties(){
+                                return {
+                                    entity: {
+                                        type: String
+                                    }
+                                }
+                            }
+                        }
+                        customElements.define('${this._fileName}', ${c})
+                    })();
+                    `,a.text().then((a)=>{const c=document.createElement('dom-module');c.id=this._fileName,c.innerHTML=`
+                        <template>
+                            ${a}
+                        </template>                    
+                    `,document.body.appendChild(c),document.head.appendChild(b)})})}}customElements.define('xtal-pattern',a)})();
---
+++ xtal-pattern.ts
@@ -0,0 +1,58 @@
+(function () {
+    /** 
+    * `xtal-pattern`
+    *
+    * Generate Polymer web component dynamically
+    * 
+    *
+    * @customElement
+    * @polymer
+    * @demo demo/index.html
+    */
+    class XtalPattern extends HTMLElement {
+        _href: string;
+        _fileName: string;
+        connectedCallback() {
+            this._href = this.getAttribute('href');
+            const splitPath = this._href.split('/');
+            this._fileName = splitPath[splitPath.length - 1].replace('.html', '');
+            this.loadURL();
+        }
+        loadURL() {
+            fetch(this._href).then(resp => {
+                const scriptTag = document.createElement('script');
+                const className = this._fileName.replace('-', '_');
+                scriptTag.innerText = `
+                    (function () {
+                        class ${className} extends Polymer.Element{
+                            static get is(){return '${this._fileName}'}
+                            static get properties(){
+                                return {
+                                    entity: {
+                                        type: String
+                                    }
+                                }
+                            }
+                        }
+                        customElements.define('${this._fileName}', ${className})
+                    })();
+                    `;
+
+                resp.text().then(markup => {
+
+                    const domModule = document.createElement('dom-module');
+                    domModule.id = this._fileName;
+                    domModule.innerHTML = `
+                        <template>
+                            ${markup}
+                        </template>                    
+                    `
+                    document.body.appendChild(domModule);
+                    document.head.appendChild(scriptTag);
+                });
+
+            })
+        }
+    }
+    customElements.define('xtal-pattern', XtalPattern);
+})();
